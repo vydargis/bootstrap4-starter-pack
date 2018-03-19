4e9b0f679dc3af039337a131137e16f932b99e6c
Vytautas Dargis <vy.dargis@gmail.com>
2018-03-19 10:22:17 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 
-// these are created tasks
 
-// Task 1. Compile sass & inject into browser
+/* GULP TASKS */ 
+
+// #1 Compile Sass and inject into browser
 gulp.task('sass', function() {
   // in array is sass files that we want to compile
   // * means that eveyrthing with .scss extention will be compiled
@@ -15,14 +16,14 @@ gulp.task('sass', function() {
 });
 
 
-// Task 2. Move JS files to src/js
+// #2 Move JS files to src/js
 gulp.task('js', function() {
   return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
   .pipe(gulp.dest("src/js"))
   .pipe(browserSync.stream());
 });
 
-// Task 3. Watch sass & server
+// #3 Watch Sass and server
 gulp.task('serve', ['sass'], function() {
   browserSync.init({
     server: "./src"
@@ -32,19 +33,19 @@ gulp.task('serve', ['sass'], function() {
   gulp.watch(["src/*html"]).on('change', browserSync.reload);
 });
 
-// Task 4. move fonts (font-awesome) folder to src
+// #4 Move fonts (font-awesome) folder to src folder
 gulp.task('fonts', function() {
   return gulp.src('node_modules/font-awesome/fonts/*')
   .pipe(gulp.dest("src/fonts"));
 });
 
 
-// Task 5. move font-awesome css to src/css
+// #5 Move font-awesome css to src/css folder
 gulp.task('fa', function() {
   return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
   .pipe(gulp.dest("src/css"));
 });
 
 
-// Task 6. default gulp task; when we run gulp it runs all the tasks we need; it runs when we type 'gulp'
+// #6 Default gulp task. To run default default Gulp task: in your terminal/command line type: "npm start"
 gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
